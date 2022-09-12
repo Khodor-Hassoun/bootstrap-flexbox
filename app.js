6ddf6eefbc151d072d9bbd82a8ca1358a711dd9f
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-12 23:00:28 +0300

    Error message when email is invalid

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const email = document.getElementById('email')
 const phone = document.getElementById("number")
 const textArea = document.getElementById('message')
 const error = document.querySelector('.error')
-
+let emailPattern = /\w{3\@\w{5}/g;
 
 
 form.addEventListener('submit',(e)=>{
     let messages = []
-    if(fullName.value.length < 6){
-        messages.push("Error")
+    if(fullName.value.length < 5){
+        messages.push("The name is invalid")
+    }
+    if(!(email.value.match(emailPattern))){
+        messages.push('Invalid email')
     }
     if(messages.length > 0){
         e.preventDefault()
