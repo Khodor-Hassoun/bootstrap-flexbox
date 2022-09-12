441a2d66d1b9e525752ba6015a4427373b8553b5
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-12 23:17:16 +0300

    Form messages are complete

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const email = document.getElementById('email')
 const phone = document.getElementById("number")
 const textArea = document.getElementById('message')
 const error = document.querySelector('.error')
-let emailPattern = /\w{3\@\w{5}/g;
 
+let emailPattern = /\w{3,}\@\w{5,}/g;
+let phonePattern1 = /961[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/g
+let phonePattern2 = /961[1-9][0-9][0-9][0-9][0-9][0-9][0-9]/g
+let textPattern = /\S{100,}/
 
+error.style.backgroundColor='red'
 form.addEventListener('submit',(e)=>{
     let messages = []
     if(fullName.value.length < 5){
@@ -14,6 +18,12 @@ form.addEventListener('submit',(e)=>{
     }
     if(!(email.value.match(emailPattern))){
         messages.push('Invalid email')
+    }
+    if (!(phone.value.match(phonePattern1) || phone.value.match(phonePattern2))){
+        messages.push('Invalid Phone')
+    }
+    if(!(textArea.value.match(textPattern))){
+        messages.push("Invalid message")
     }
     if(messages.length > 0){
         e.preventDefault()
