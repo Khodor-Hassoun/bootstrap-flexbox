44e873fbf72a2bb0461f1e1e5572577800c7c8a0
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-13 22:45:45 +0300

    Successful post request from the post to the database

---
+++ apis/addContact.php
@@ -0,0 +1,20 @@
+<?php
+include("connection.php");
+
+$name = $_POST["fullname"];
+$email = $_POST["email"];
+$phone = $_POST["number"];
+$message = $_POST["message"];
+
+// $query = "INSERT INTO contact_info(name, author) VALUE (" . $name .", ?)";
+
+$query = $mysqli->prepare("INSERT INTO contact_info(name, email, phone, message) VALUE (?, ?, ?, ?)");
+$query->bind_param("ssis", $name, $email, $phone, $message);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
--- app.js
+++ app.js
@@ -10,23 +10,23 @@ let phonePattern1 = /961[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/g
 let phonePattern2 = /961[1-9][0-9][0-9][0-9][0-9][0-9][0-9]/g
 let textPattern = /\S{100,}/
 
-error.style.backgroundColor='red'
-form.addEventListener('submit',(e)=>{
-    let messages = []
-    if(fullName.value.length < 5){
-        messages.push("The name is invalid")
-    }
-    if(!(email.value.match(emailPattern))){
-        messages.push('Invalid email')
-    }
-    if (!(phone.value.match(phonePattern1) || phone.value.match(phonePattern2))){
-        messages.push('Invalid Phone')
-    }
-    if(!(textArea.value.match(textPattern))){
-        messages.push("Invalid message")
-    }
-    if(messages.length > 0){
-        e.preventDefault()
-        error.innerText = messages.join(',')
-    }
-})
+// error.style.backgroundColor='red'
+// form.addEventListener('submit',(e)=>{
+//     let messages = []
+//     if(fullName.value.length < 5){
+//         messages.push("The name is invalid")
+//     }
+//     if(!(email.value.match(emailPattern))){
+//         messages.push('Invalid email')
+//     }
+//     if (!(phone.value.match(phonePattern1) || phone.value.match(phonePattern2))){
+//         messages.push('Invalid Phone')
+//     }
+//     if(!(textArea.value.match(textPattern))){
+//         messages.push("Invalid message")
+//     }
+//     if(messages.length > 0){
+//         e.preventDefault()
+//         error.innerText = messages.join(',')
+//     }
+// })
